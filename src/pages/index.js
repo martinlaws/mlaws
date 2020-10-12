7e67b5781b59ccae6cb5fee2a4326dfc557e6940
Martin <martin.laws@shopify.com>
2020-10-11 20:04:15 -0400

    Removed comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 import Splash from "../components/splash"
-// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
